3dd445844325a01b30c99328580d93296974b5ef
Amir <sahnaseredini@aut.ac.ir>
2019-09-28 10:56:48 +0100

    Initialising webpage

---
+++ Tmp/index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+	<head>
+		<title>Homepage</title>
+		<meta charset="UTF-8">
+		<meta name="description" content="Homepage">
+		<meta name="keywords" content="resume, aut, information security, Seyed Amir Hossain Naseredini">
+		<meta name="author" content="Seyed Amir Hossain Naseredini">
+		</head>
+	<body>
+		<p style="color:Black; font-size:50px; text-align:center; ">Sorry, this website is under the construction!</p>
+	</body>
+</html>
